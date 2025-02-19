0b72295340018c908b56a7217153ba08d62adc70
grommoott <grommoott@gmail.com>
2025-02-19 13:03:09 +0700

    backend: add GET /rooms/list

--- backend/src/rooms/rooms.controller.ts
+++ backend/src/rooms/rooms.controller.ts
@@ -1,16 +1,23 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "src/tokens/auth.guard";
-import { IRoom } from "./interfaces/room.interface";
-import { CreateRoomDto } from "./dto/createRoom.dto";
-import { RoomsService } from "./rooms.service";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common"
+import { AuthGuard } from "src/tokens/auth.guard"
+import { IRoom } from "./interfaces/room.interface"
+import { CreateRoomDto } from "./dto/createRoom.dto"
+import { RoomsService } from "./rooms.service"
+import { User } from "src/users/users.entity"
 
 @Controller("rooms")
 export class RoomsController {
-    constructor(private roomsService: RoomsService) { }
+    constructor(private roomsService: RoomsService) {}
 
     @UseGuards(AuthGuard)
     @Post("create")
     async create(@Body() createRoomDto: CreateRoomDto): Promise<IRoom> {
         return await this.roomsService.createRoom(createRoomDto)
+    }
+
+    @UseGuards(AuthGuard)
+    @Get("list")
+    async list(@Body("user") user: User): Promise<IRoom[]> {
+        return await this.roomsService.getRooms(user)
     }
 }
--- backend/src/rooms/rooms.service.ts
+++ backend/src/rooms/rooms.service.ts
@@ -1,25 +1,42 @@
-import { ForbiddenException, Injectable, InternalServerErrorException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Room } from "./rooms.entity";
-import { Repository } from "typeorm";
-import { CreateRoomDto } from "./dto/createRoom.dto";
-import { IRoom } from "./interfaces/room.interface";
-import { ForbiddenErrors } from "src/errors";
-import { Role } from "src/roles/roles.entity";
-import { HashService } from "src/helpers/hash.service";
-import { Roles } from "src/roles/enums/role.enum";
-import { RoomStates } from "./enums/roomState.enum";
+import {
+    ForbiddenException,
+    Injectable,
+    InternalServerErrorException,
+} from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Room } from "./rooms.entity"
+import { Repository } from "typeorm"
+import { CreateRoomDto } from "./dto/createRoom.dto"
+import { IRoom } from "./interfaces/room.interface"
+import { ForbiddenErrors } from "src/errors"
+import { Role } from "src/roles/roles.entity"
+import { HashService } from "src/helpers/hash.service"
+import { Roles } from "src/roles/enums/role.enum"
+import { RoomStates } from "./enums/roomState.enum"
+import { User } from "src/users/users.entity"
 
 @Injectable()
 export class RoomsService {
-    private readonly selectorIRoom = { roles: { role: true, user: { name: true } }, name: true, state: true, time: true }
+    private readonly selectorIRoom = {
+        roles: { role: true, user: { name: true } },
+        name: true,
+        state: true,
+        time: true,
+    }
+    private readonly relationsIRoom = { roles: { user: true } }
 
-    constructor(@InjectRepository(Room) private roomsRepository: Repository<Room>,
+    constructor(
+        @InjectRepository(Room) private roomsRepository: Repository<Room>,
         @InjectRepository(Role) private rolesRepository: Repository<Role>,
-        private hashService: HashService) { }
+        private hashService: HashService,
+    ) {}
 
     async createRoom(createRoomDto: CreateRoomDto): Promise<IRoom> {
-        if (await this.roomsRepository.findOneBy({ name: createRoomDto.name }) != null) {
+        if (
+            (await this.roomsRepository.findOneBy({
+                name: createRoomDto.name,
+            })) != null
+        ) {
             throw new ForbiddenException(ForbiddenErrors.RoomAlreadyExists)
         }
 
@@ -36,17 +53,24 @@ export class RoomsService {
         room.roles = [role]
         await this.roomsRepository.save(room)
 
-        const roomWithRoles = await this.roomsRepository
-            .findOne({
-                where: { name: room.name },
-                relations: { roles: { user: true } },
-                select: this.selectorIRoom
-            })
+        const roomWithRoles = await this.roomsRepository.findOne({
+            where: { name: room.name },
+            relations: this.relationsIRoom,
+            select: this.selectorIRoom,
+        })
 
         if (roomWithRoles == null) {
             throw new InternalServerErrorException()
         }
 
         return roomWithRoles
+    }
+
+    async getRooms(user: User): Promise<IRoom[]> {
+        return await this.roomsRepository.find({
+            where: { roles: { user } },
+            relations: this.relationsIRoom,
+            select: this.selectorIRoom,
+        })
     }
 }
